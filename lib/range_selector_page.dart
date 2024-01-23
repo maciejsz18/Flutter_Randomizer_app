7d7ac8e262d8a6207969bbef176181ccb064e48c
maciejsz18 <maciej.szczudlo@onet.com.pl>
2024-01-23 18:20:37 +0100

    implemented randomizer page

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:randomizer/randomizer_page.dart';
+import 'package:randomizer/range_selector_form.dart';
 
 class RangeSelectorPage extends StatefulWidget {
   const RangeSelectorPage({super.key});
@@ -18,68 +20,27 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
       appBar: AppBar(
         title: const Text('Select Range'),
       ),
-      body: Form(
-        key: formKey,
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              RangeSelectorTextFormField(
-                labelText: 'Minimum',
-                intValueSetter: (value) => _min = value,
-              ),
-              SizedBox(height: 12.0),
-              RangeSelectorTextFormField(
-                labelText: 'Maximum',
-                intValueSetter: (value) => _max = value,
-              ),
-            ],
-          ),
-        ),
+      body: RangeSelectorForm(
+        formKey: formKey,
+        minValueSetter: (value) => _min = value,
+        maxValueSetter: (value) => _max = value,
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_forward),
         onPressed: () {
           if (formKey.currentState?.validate() == true) {
             formKey.currentState?.save();
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => RandomizerPage(
+                  min: _min,
+                  max: _max,
+                ),
+              ),
+            );
           }
-          // TODO: Navigate to the generator page
         },
       ),
-    );
-  }
-}
-
-class RangeSelectorTextFormField extends StatelessWidget {
-  const RangeSelectorTextFormField({
-    super.key,
-    required this.labelText,
-    required this.intValueSetter,
-  });
-
-  final String labelText;
-  final void Function(int value) intValueSetter;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(),
-        labelText: labelText,
-      ),
-      keyboardType: const TextInputType.numberWithOptions(
-        decimal: false,
-        signed: true,
-      ),
-      validator: (value) {
-        if (value == null || int.tryParse(value) == null) {
-          return 'Please enter a valid integer';
-        } else {
-          return null;
-        }
-      },
-      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? '')),
     );
   }
 }
